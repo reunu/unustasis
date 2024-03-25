2179ba54c977148b442a85247be49a2db498fb5f
Freal <leonard.wohlfarth@gmail.com>
2024-03-26 00:01:36 +0100

    Add bluetooth libraries and quick&ugly implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:unustasis/no_scooter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +12,210 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool linked = false, scanning = false;
+  List<BluetoothDevice> foundScooters = [];
+  BluetoothDevice? myScooter;
+  String debugText = "";
 
-  void _incrementCounter() {
+  void _startScanning() async {
+    for (var device in FlutterBluePlus.connectedDevices) {
+      if (device.advName == "unu Scooter" && !foundScooters.contains(device)) {
+        foundScooters.add(device);
+      }
+    }
+    myScooter = null;
+    // start scan
+    FlutterBluePlus.startScan();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      scanning = true;
+    });
+    var subscription = FlutterBluePlus.onScanResults.listen(
+      (results) {
+        if (results.isNotEmpty) {
+          ScanResult r = results.last; // the most recently found device
+          log('${r.device.remoteId}: "${r.advertisementData.advName.isNotEmpty ? r.advertisementData.advName : "unnamed"}" found!');
+          if (r.advertisementData.advName == "unu Scooter" &&
+              !foundScooters.contains(r.device)) {
+            foundScooters.add(r.device);
+          }
+        }
+      },
+      onError: (e) => log(e.toString()),
+    );
+    FlutterBluePlus.cancelWhenScanComplete(subscription);
+    Future.delayed(const Duration(seconds: 10), () {
+      FlutterBluePlus.stopScan();
+      setState(() {
+        scanning = false;
+      });
+    });
+  }
+
+  void _connect(BluetoothDevice scooter) async {
+    log("Connecting to ${scooter.advName}...");
+    if (scooter.isConnected) {
+      log("Already connected!");
+      await scooter.discoverServices();
+      setState(() {
+        linked = true;
+        myScooter = scooter;
+      });
+      return;
+    }
+    try {
+      await scooter.connect(
+        timeout: const Duration(seconds: 10),
+      );
+      log("Linked to ${scooter.advName}!");
+      await scooter.discoverServices();
+      setState(() {
+        linked = true;
+        myScooter = scooter;
+      });
+    } catch (e) {
+      setState(() {
+        debugText = e.toString();
+      });
+      log(e.toString());
+      return;
+    }
+  }
+
+  void _unlock() async {
+    await myScooter?.discoverServices();
+    setState(() {
+      debugText = "Unlocking...";
+    });
+    myScooter?.servicesList.forEach((service) {
+      log("Service: ${service.uuid.toString()}");
+      service.characteristics.forEach((characteristic) {
+        log("Characteristic: ${characteristic.uuid.toString()}");
+        if (service.uuid.toString() == "9A590000-6E67-5D0D-AAB9-AD9126B66F91" &&
+            characteristic.uuid.toString() ==
+                "9A590001-6E67-500D-AAB9-AD9126866F91") {
+          setState(() {
+            debugText = "Found characteristic...";
+          });
+          characteristic.write(ascii.encode("scooter:state unlock"),
+              withoutResponse: true);
+        }
+      });
+    });
+  }
+
+  void _lock() async {
+    await myScooter?.discoverServices(); // this is so redundant
+    myScooter?.servicesList.forEach((service) {
+      log("Service: ${service.uuid.toString()}");
+      service.characteristics.forEach((characteristic) {
+        log("Characteristic: ${characteristic.uuid.toString()}");
+        if (service.uuid.toString() == "9A590000-6E67-5D0D-AAB9-AD9126B66F91" &&
+            characteristic.uuid.toString() ==
+                "9A590001-6E67-500D-AAB9-AD9126866F91") {
+          log("Unlocking...");
+          characteristic.write(ascii.encode("scooter:state lock"),
+              withoutResponse: true);
+        }
+      });
     });
   }
 
   @override
+  void initState() {
+    _startScanning();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Unustasis"),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              ListView(
+                shrinkWrap: true,
+                children: foundScooters.isEmpty
+                    ? [NoScooterMsg(scanning: scanning)]
+                    : foundScooters.map((scooter) {
+                        return ListTile(
+                          title: Text(
+                            scooter.advName +
+                                (scooter.isConnected ? " (connected)" : ""),
+                            textAlign: TextAlign.center,
+                          ),
+                          subtitle: Text(
+                            scooter.remoteId.toString(),
+                            textAlign: TextAlign.center,
+                          ),
+                          onTap: () => _connect(scooter),
+                          tileColor: scooter == myScooter
+                              ? Theme.of(context)
+                                  .colorScheme
+                                  .primary
+                                  .withOpacity(0.5)
+                              : Colors.grey.shade300,
+                        );
+                      }).toList(),
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton.icon(
+                onPressed: myScooter == null ? null : _unlock,
+                icon: const Icon(Icons.lock_open),
+                label: const Text("Unlock"),
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton.icon(
+                onPressed: myScooter == null ? null : _lock,
+                icon: const Icon(Icons.lock),
+                label: const Text("Lock"),
+              ),
+              const SizedBox(height: 32),
+              Text(
+                '''Linked: $linked,
+Scanning: $scanning,
+Found: ${foundScooters.length},
+My scooter: ${myScooter?.remoteId.toString() ?? "none"}''',
+                textAlign: TextAlign.center,
+              ),
+              Text(debugText),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: scanning ? null : _startScanning,
+        tooltip: 'Refresh',
+        child: const Icon(Icons.refresh),
+      ),
     );
   }
 }
