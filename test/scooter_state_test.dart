9597447fc8f05d79de2c1fc370463f7531e1c438
Max <dsiw@dsiw-it.de>
2024-04-29 08:57:24 +0200

    Add unit tests for isOn and isReadyForLockChange

--- test/scooter_state_test.dart
+++ test/scooter_state_test.dart
@@ -21,5 +21,33 @@ void main() {
       expect(ScooterState.fromStateAndPowerState('', ''), equals(ScooterState.unknown));
       expect(ScooterState.fromStateAndPowerState('unknown', ''), equals(ScooterState.unknown));
     });
+
+    test('isOn', () {
+      expect(ScooterState.standby.isOn, false);
+      expect(ScooterState.off.isOn, false);
+      expect(ScooterState.parked.isOn, true);
+      expect(ScooterState.shuttingDown.isOn, false);
+      expect(ScooterState.ready.isOn, true);
+      expect(ScooterState.hibernating.isOn, false);
+      expect(ScooterState.hibernatingImminent.isOn, false);
+      expect(ScooterState.booting.isOn, false);
+      expect(ScooterState.unknown.isOn, false);
+      expect(ScooterState.linking.isOn, false);
+      expect(ScooterState.disconnected.isOn, false);
+    });
+
+    test('isReadyForLockChange', () {
+      expect(ScooterState.standby.isReadyForLockChange, true);
+      expect(ScooterState.off.isReadyForLockChange, true);
+      expect(ScooterState.parked.isReadyForLockChange, true);
+      expect(ScooterState.shuttingDown.isReadyForLockChange, false);
+      expect(ScooterState.ready.isReadyForLockChange, true);
+      expect(ScooterState.hibernating.isReadyForLockChange, true);
+      expect(ScooterState.hibernatingImminent.isReadyForLockChange, true);
+      expect(ScooterState.booting.isReadyForLockChange, false);
+      expect(ScooterState.unknown.isReadyForLockChange, false);
+      expect(ScooterState.linking.isReadyForLockChange, false);
+      expect(ScooterState.disconnected.isReadyForLockChange, false);
+    });
   });
 }
