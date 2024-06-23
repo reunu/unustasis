681cd99d783e22c778f555d3e8fc1d5375634e2a
Max <dsiw@dsiw-it.de>
2024-06-23 22:57:59 +0200

    Use wear_plus

--- lib/interfaces/wear/home_screen_watch.dart
+++ lib/interfaces/wear/home_screen_watch.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unustasis/scooter_service.dart';
+import 'package:wear_plus/wear_plus.dart';
 
 class HomeScreenWatch extends StatelessWidget {
   final ScooterService scooterService;
@@ -13,10 +14,31 @@ class HomeScreenWatch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text("WEAR"),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: WatchShape(
+            builder: (BuildContext context, WearShape shape, Widget? child) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Shape: ${shape == WearShape.round ? 'round' : 'square'}',
+                  ),
+                  child!,
+                ],
+              );
+            },
+            child: AmbientMode(
+              builder: (BuildContext context, WearMode mode, Widget? child) {
+                return Text(
+                  'Mode: ${mode == WearMode.active ? 'Active' : 'Ambient'}',
+                );
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
