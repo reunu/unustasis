3fd5c66c2a005ecf7d6c56bea50962cae5bc6bad
Freal <leonard.wohlfarth@gmail.com>
2024-12-13 17:00:36 +0100

    Adding improved support screen with garage finder

---
+++ lib/stats/support_section.dart
@@ -0,0 +1,371 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
+import 'package:maps_launcher/maps_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../control_screen.dart';
+import '../domain/log_helper.dart';
+
+class SupportSection extends StatefulWidget {
+  const SupportSection({super.key});
+
+  @override
+  State<SupportSection> createState() => _SupportSectionState();
+}
+
+class _SupportSectionState extends State<SupportSection> {
+  List<Widget> supportItems() => [
+        Header("Common questions"),
+        const FaqWidget(),
+        Header("Garages"),
+        const GarageWidget(),
+        Header("Get support"),
+        ListTile(
+          leading: const Icon(Icons.bug_report_outlined),
+          title: Text("Report a bug"),
+          trailing: const Icon(Icons.chevron_right),
+          onTap: () {
+            LogHelper.startBugReport(context);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.discord_outlined),
+          title: const Text("Unu Community"),
+          trailing: const Icon(Icons.chevron_right),
+          onTap: () {
+            launchUrl(Uri.parse("https://discord.gg/UEPGY8AG9V"));
+          },
+        ),
+      ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      shrinkWrap: true,
+      itemCount: supportItems().length,
+      separatorBuilder: (context, index) => Divider(
+        indent: 16,
+        endIndent: 16,
+        height: 24,
+        color: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+      ),
+      itemBuilder: (context, index) => supportItems()[index],
+    );
+  }
+}
+
+class FaqWidget extends StatelessWidget {
+  const FaqWidget({super.key});
+
+  // get the right FAQs
+  Future<Map<String, dynamic>> getSupportMap({
+    required BuildContext context,
+    required String languageCode,
+  }) async {
+    String data = await DefaultAssetBundle.of(context)
+        .loadString("assets/faq_$languageCode.json");
+    return jsonDecode(data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getSupportMap(
+          context: context,
+          languageCode: FlutterI18n.currentLocale(context)!.languageCode,
+        ),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: SizedBox(
+                height: 40,
+                width: 40,
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          Map<String, dynamic> faq = snapshot.data!;
+          return ListView.separated(
+            shrinkWrap: true,
+            itemCount: faq.length,
+            separatorBuilder: (context, index) => Divider(
+              indent: 16,
+              endIndent: 16,
+              height: 24,
+              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+            ),
+            itemBuilder: (context, index) {
+              MapEntry category = faq.entries.elementAt(index);
+              return ExpansionTile(
+                shape: const RoundedRectangleBorder(),
+                initiallyExpanded: false,
+                iconColor: Theme.of(context).colorScheme.onSurface,
+                leading: index == 0
+                    ? const Icon(Icons.bluetooth)
+                    : index == 1
+                        ? const Icon(Icons.question_answer_outlined)
+                        : const Icon(Icons.info),
+                tilePadding: const EdgeInsets.only(left: 20, right: 20),
+                title: Text(
+                  category.key.toString(),
+                ),
+                children: [
+                  for (MapEntry question in category.value.entries)
+                    ExpansionTile(
+                        shape: const RoundedRectangleBorder(),
+                        iconColor: Theme.of(context).colorScheme.onSurface,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.surfaceContainer,
+                        tilePadding: const EdgeInsets.only(
+                            left: 32, right: 16, top: 8, bottom: 8),
+                        title: Text(question.key.toString()),
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(32, 0, 16, 32),
+                            child: Text(
+                              question.value.toString(),
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurface
+                                      .withOpacity(0.6)),
+                            ),
+                          )
+                        ]),
+                ],
+              );
+            },
+          );
+        });
+  }
+}
+
+class GarageWidget extends StatelessWidget {
+  const GarageWidget({super.key});
+
+  Future<List<Garage>> getGarages() async {
+    print("Getting garages from API");
+    final response = await http
+        .get(Uri.parse('https://unumotors.com/page-data/sq/d/2596243890.json'));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+      Map<String, dynamic> data = json['data'];
+      Map<String, dynamic> content = data['contentfulGaragesJson'];
+      List<dynamic> garages = content['garages'];
+      return garages.map((garage) => Garage.fromJson(garage)).toList();
+    } else {
+      throw Exception('Failed to load garages');
+    }
+  }
+
+  Future<List<Garage>> getClosestGarages() async {
+    // check for GPS permission
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw const PermissionDeniedException(
+            "Location permissions are/were denied");
+      }
+    }
+    // get both location and garage data
+    List<dynamic> result = await Future.wait({
+      getGarages(),
+      Geolocator.getCurrentPosition(),
+    });
+    // organize results
+    List<Garage> garages = result[0] as List<Garage>;
+    Position currentPosition = result[1] as Position;
+
+    // set the distance of each garage
+    for (Garage garage in garages) {
+      double distance = Geolocator.distanceBetween(
+          currentPosition.latitude,
+          currentPosition.longitude,
+          garage.location.latitude,
+          garage.location.longitude);
+      garage.distance = distance;
+    }
+    // sort by distance
+    garages.sort((a, b) => a.distance!.compareTo(b.distance!));
+    // only return the 3 closest garages
+    return garages.take(5).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      child: FutureBuilder(
+        future: getClosestGarages(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(Icons.search_off_outlined, size: 40),
+                const SizedBox(height: 8),
+                Text(FlutterI18n.translate(context, "stats_no_garages")),
+              ],
+            );
+          }
+          if (!snapshot.hasData) {
+            return const Center(
+              child: SizedBox(
+                height: 40,
+                width: 40,
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          List<Garage> garages = snapshot.data!;
+          return ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            scrollDirection: Axis.horizontal,
+            itemCount: garages.length,
+            itemBuilder: (context, index) =>
+                _GarageTile(garage: garages[index]),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _GarageTile extends StatelessWidget {
+  const _GarageTile({
+    required this.garage,
+  });
+
+  final Garage garage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16),
+      child: Container(
+        width: 300,
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceContainer,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(garage.name, style: Theme.of(context).textTheme.titleLarge),
+            const SizedBox(height: 4),
+            Text((garage.distance! / 1000).toStringAsFixed(2) + " km"),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: TextButton.icon(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.onSurface,
+                      foregroundColor: Theme.of(context).colorScheme.surface,
+                    ),
+                    onPressed: () {
+                      MapsLauncher.launchQuery(
+                          "${garage.name} ${garage.street}, ${garage.zipCode}");
+                    },
+                    label: Text("Map"),
+                    icon: Icon(Icons.map_outlined),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: TextButton.icon(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.onSurface,
+                      foregroundColor: Theme.of(context).colorScheme.surface,
+                    ),
+                    onPressed: () {
+                      launchUrl(Uri(
+                        scheme: 'tel',
+                        path: garage.phone,
+                      ));
+                    },
+                    label: Text("Call"),
+                    icon: Icon(Icons.phone_outlined),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Garage {
+  String name;
+  String phone;
+  String street;
+  String city;
+  String country;
+  String countryCode;
+  String zipCode;
+  LatLng location;
+  double? distance;
+
+  Garage({
+    required this.name,
+    required this.phone,
+    required this.street,
+    required this.city,
+    required this.country,
+    required this.countryCode,
+    required this.zipCode,
+    required this.location,
+  });
+
+  factory Garage.fromJson(Map<String, dynamic> json) {
+    try {
+      return Garage(
+        name: json["name"]?.isNotEmpty == true ? json['name'] : "Unnamed",
+        phone: json["Phone"]?.isNotEmpty == true
+            ? json['Phone'].toString()
+            : "Unknown",
+        street: json["ShippingStreet"]?.isNotEmpty == true
+            ? json['ShippingStreet']
+            : "Unknown street",
+        city: json["ShippingCity"]?.isNotEmpty == true
+            ? json['ShippingCity']
+            : "Unknown city",
+        country: json["ShippingCountry"]?.isNotEmpty == true
+            ? json['ShippingCountry']
+            : "Unknown country",
+        countryCode: json["ShippingCountryCode"]?.isNotEmpty == true
+            ? json['ShippingCountryCode']
+            : "??",
+        zipCode: json["ShippingPostalCode"]?.isNotEmpty == true
+            ? json['ShippingPostalCode'].toString()
+            : "?????",
+        location: LatLng(
+          double.parse(json['ShippingLatitude']?.isNotEmpty == true
+              ? json['ShippingLatitude']
+              : "0"),
+          double.parse(json['ShippingLongitude']?.isNotEmpty == true
+              ? json['ShippingLongitude']
+              : "0"),
+        ),
+      );
+    } catch (e) {
+      Logger("Garage").severe("Malformed garage", e);
+      Logger("Garage").severe(e.toString());
+      Logger("Garage").severe(json.toString());
+      rethrow;
+    }
+  }
+}
