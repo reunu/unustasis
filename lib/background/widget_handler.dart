3eacbfb755a0da24066677796e659a1c77db6ac8
Freal <leonard.wohlfarth@gmail.com>
2025-05-25 17:24:16 +0200

    No minutely updates for lastping labels

--- lib/background/widget_handler.dart
+++ lib/background/widget_handler.dart
@@ -211,8 +211,6 @@ extension DateTimeExtension on DateTime {
       return '${difference.inDays}d';
     } else if (difference.inHours >= 1) {
       return '${difference.inHours}h';
-    } else if (difference.inMinutes >= 1) {
-      return '${difference.inMinutes}min';
     } else {
       return null;
     }
